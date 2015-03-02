afa4867119c012a037daa4a7ab595ce456b302ba
wangshauilong <oopswangsl@gmail.com>
2015-03-03 00:50:51 +0800

    change repo

--- casestudy/US_Homeinsurance_Rates/us_homeinsurance_rate.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-Insurance_rates = pd.read_csv('US_Homeinsurance_Rates.csv', delimiter = ',')
-
-Insurance_rates.sort (['2013', 'state'], ascending = [1, 0])
-
-Insurance_rates ['Percentage Change'] = (Insurance_rates ['2013']-Insurance_rates ['2003'])*100 / Insurance_rates ['2013']
-
-print Insurance_rates.loc[:,['state','2003','2013','Percentage Change']]
